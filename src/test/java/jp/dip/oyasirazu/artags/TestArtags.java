44bbfa3b9cc739bd603d45337bae0bfee36abeaf
mikoto2000 <mikoto2000@gmail.com>
2017-05-02 01:56:04 +0900

    Artags テストクラスのひな型追加。

---
+++ src/test/java/jp/dip/oyasirazu/artags/TestArtags.java
@@ -0,0 +1,52 @@
+package jp.dip.oyasirazu.artags;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Set;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.xpath.XPathExpressionException;
+
+import jp.dip.oyasirazu.artags.Artags.Arxml;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+import static org.junit.Assert.*;
+
+/**
+ * TestArtags
+ * TODO: テストバリエーションをまじめに考える
+ */
+public class TestArtags {
+
+    @Test
+    public void testFindArxmls() {
+        fail();
+    }
+
+    @Test
+    public void testFindArxmlsWithExclude() {
+        fail();
+    }
+
+    @Test
+    public void testCreateTagsString() {
+        fail();
+    }
+
+    @Before
+    public void setup() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+}
+
